2f08cdd02ae68ea5095530f0cedb103164ca9cf9
rfsx0829 <rfsx0829@163.com>
2019-06-01 19:18:00 +0800

    fix bug

--- server/plat/plat.go
+++ server/plat/plat.go
@@ -27,14 +27,13 @@ func New(db redis.Client) *Platform {
 		waitConn:  make([]int, 0, 2),
 		database:  db,
 	}
-	// go p.logerr()
 
-	p.RoomTable[1] = &Room{
+	/* p.RoomTable[1] = &Room{
 		ID:     1,
 		Name:   "DefaultRoom",
 		Nums:   0,
 		inRoom: make([]*User, 0, 2),
-	}
+	} */
 
 	return p
 }
@@ -46,8 +45,10 @@ func (p *Platform) routine(id int, conn *websocket.Conn) {
 		err := conn.ReadJSON(&x)
 		if err != nil {
 			log.Println("[routine] Error. Will Delete Conn.", err)
-			p.UserTable[id].inWhichRoom.removeUser(id)
-			p.UserTable[id].inWhichRoom = nil
+			if p.UserTable[id].inWhichRoom != nil {
+				p.UserTable[id].inWhichRoom.removeUser(id)
+				p.UserTable[id].inWhichRoom = nil
+			}
 			p.ConnPool[id] = nil
 			return
 		}
